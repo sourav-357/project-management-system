1c8f88aa147ad4f95376846da57d4354299c5978
Sourav Kumar <xsouravkumar357@gmail.com>
2026-07-19 23:37:08 +0530

    project model completed

---
+++ server/controllers/student.controller.js
@@ -0,0 +1,11 @@
+
+
+
+import { asyncHandler } from '../middlewares/asyncHandler.js';
+import ErrorHandler from '../middlewares/error.js';
+import { User } from '../models/user.js';
+import * as userService from '../services/userService.js';
+
+
+
+export const 
