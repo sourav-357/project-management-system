7121660ae1e1fe555f27fcc31f2a04443e8aba06
Sourav Kumar <xsouravkumar357@gmail.com>
2026-07-19 23:46:28 +0530

    notification schema completed

---
+++ server/models/notification.js
@@ -0,0 +1,51 @@
+
+
+
+
+import mongoose from 'mongoose';
+
+
+
+const notificationSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, 'user ID is required field'],
+    },
+    message: {
+        type: String,
+        required: [true, 'message is required field'],
+        trim: true,
+        maxLength: [1000, 'message cannot exceed 1000 characters'],
+    },
+    isRead: {
+        type: Boolean,
+        default: false
+    },
+    link: {
+        type: String,
+        default: null
+    },
+    type: {
+        type: String,
+        enum: ['request', 'approval', 'feedback', 'rejection', 'deadline', 'general', 'meeting', 'system'],
+        default: 'general'
+    },
+    priority: {
+        type: String,
+        enum: ['low', 'medium', 'high'],
+        default: 'low'
+    }
+}, { timestamps: true }
+);
+
+
+
+// * indexing for better performance
+projectSchema.index({ user: 1 });
+projectSchema.index({ isRead: 1 });
+
+
+
+export const Project = mongoose.models.Notification || mongoose.model('Notification', notificationSchema);
+
