6b06fc863b4d1abe774d0fa17c045080e0461de9
Sourav Kumar <xsouravkumar357@gmail.com>
2026-07-24 04:58:23 +0530

    error console

--- server/config/db.js
+++ server/config/db.js
@@ -7,6 +7,7 @@ import { seedDefaultUsers } from './seed.js';
 
 export const connectDB = async () => {
     try {
+        console.log("Connecting to MongoDB...");
         await mongoose.connect(process.env.MONGO_URI);
         console.log('database connected successfully');
         await seedDefaultUsers();
