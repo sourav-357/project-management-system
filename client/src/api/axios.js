f9a950849e1ff379e45ee9226e68c21164eaab77
Sourav Kumar <xsouravkumar357@gmail.com>
2026-07-24 05:34:55 +0530

    fixed frontend api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -9,7 +9,7 @@ export const setAccessToken = (token) => {
 export const getAccessToken = () => inMemoryToken;
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
